430a3778058ef526358fe2d13e30b458b4f576ed
Raj Kumar <62750924+rajv69023@users.noreply.github.com>
2025-10-19 13:21:01 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,4 +1,4 @@
-class Simple{  
+class Helloworld{  
     public static void main(String args[]){  
      System.out.println("Hello Raj this your repo");
       System.out.println("devops engineer");
